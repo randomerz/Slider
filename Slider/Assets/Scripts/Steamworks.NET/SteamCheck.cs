46b672f344aca91a335dcccfa72c54f3f862cc25
Drew <46387438+Abnormal202@users.noreply.github.com>
2021-12-23 22:43:11 -0500

    Add basic Steamworks Integration

---
+++ Slider/Assets/Scripts/Steamworks.NET/SteamCheck.cs
@@ -0,0 +1,21 @@
+using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!SteamManager.Initialized)
+        {
+            Debug.LogWarning("Steam Manager is Not Initialized! Make sure Steam is running in the background if you want to test Steam-related features!");
+        }
+        else
+        {
+            Debug.Log("Hello Steam user " + SteamFriends.GetPersonaName());
+        }
+    }
+
+}
