92cf3abfe44604801bbe060175d740d5b4d042bd
boomo <randomerz56@gmail.com>
2022-01-09 02:10:49 -0500

    added fmod to repo

---
+++ Slider/Assets/Plugins/FMOD/src/Editor/EventCache.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FMODUnity
+{
+    class EventCache : ScriptableObject
+    {
+        public static int CurrentCacheVersion = 10;
+
+        [SerializeField]
+        public List<EditorBankRef> EditorBanks;
+        [SerializeField]
+        public List<EditorEventRef> EditorEvents;
+        [SerializeField]
+        public List<EditorParamRef> EditorParameters;
+        [SerializeField]
+        public List<EditorBankRef> MasterBanks;
+        [SerializeField]
+        public List<EditorBankRef> StringsBanks;
+        [SerializeField]
+        Int64 cacheTime;
+        [SerializeField]
+        public int cacheVersion;
+
+        public DateTime CacheTime
+        {
+            get { return new DateTime(cacheTime); }
+            set { cacheTime = value.Ticks; }
+        }
+
+        public EventCache()
+        {
+            EditorBanks = new List<EditorBankRef>();
+            EditorEvents = new List<EditorEventRef>();
+            EditorParameters = new List<EditorParamRef>();
+            MasterBanks = new List<EditorBankRef>();
+            StringsBanks = new List<EditorBankRef>();
+            cacheTime = 0;
+        }
+    }
+}
---
+++ Slider/Assets/Plugins/FMOD/src/Editor/StudioGlobalParameterTriggerEditor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace FMODUnity
+{
+    [CustomEditor(typeof(StudioGlobalParameterTrigger))]
+    public class StudioGlobalParameterTriggerEditor : Editor
+    {
+        SerializedProperty param;
+        SerializedProperty trigger;
+        SerializedProperty tag;
+        SerializedProperty value;
+
+        SerializedProperty data1, data2;
+
+        static GUIContent NotFoundWarning;
+
+        string currentPath;
+
+        [SerializeField]
+        EditorParamRef editorParamRef;
+
+        void OnEnable()
+        {
+            param = serializedObject.FindProperty("parameter");
+            trigger = serializedObject.FindProperty("TriggerEvent");
+            tag = serializedObject.FindProperty("CollisionTag");
+            value = serializedObject.FindProperty("value");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (NotFoundWarning == null)
+            {
+                Texture warningIcon = EditorUtils.LoadImage("NotFound.png");
+                NotFoundWarning = new GUIContent("Parameter Not Found", warningIcon);
+            }
+
+            EditorGUILayout.PropertyField(trigger, new GUIContent("Trigger"));
+            if (trigger.enumValueIndex >= (int)EmitterGameEvent.TriggerEnter && trigger.enumValueIndex <= (int)EmitterGameEvent.TriggerExit2D)
+            {
+                tag.stringValue = EditorGUILayout.TagField("Collision Tag", tag.stringValue);
+            }
+
+            EditorGUILayout.PropertyField(param, new GUIContent("Parameter"));
+
+            if (param.stringValue != currentPath)
+            {
+                currentPath = param.stringValue;
+
+                if (string.IsNullOrEmpty(param.stringValue))
+                {
+                    editorParamRef = null;
+                }
+                else
+                {
+                    editorParamRef = EventManager.ParamFromPath(param.stringValue);
+                    value.floatValue = Mathf.Clamp(value.floatValue, editorParamRef.Min, editorParamRef.Max);
+                }
+            }
+
+            if (editorParamRef != null)
+            {
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    EditorGUILayout.PrefixLabel("Override Value");
+                    value.floatValue = EditorUtils.DrawParameterValueLayout(value.floatValue, editorParamRef);
+                }
+            }
+            else
+            {
+                Rect rect = EditorGUILayout.GetControlRect();
+                rect.xMin += EditorGUIUtility.labelWidth;
+
+                GUI.Label(rect, NotFoundWarning);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
