b24553845cef4d6457ff5bc78c3928f2348e2691
aVz92 <aneesh.boreda92@gmail.com>
2022-01-14 16:18:31 -0500

    rotating works but needs testing
    something changed about fmod hopefully nothing bad

--- Slider/Assets/Scripts/UI/Artifact/UIArtifact.cs
+++ Slider/Assets/Scripts/UI/Artifact/UIArtifact.cs
@@ -197,12 +197,27 @@ public class UIArtifact : MonoBehaviour
     // temporary
     public void RotateTiles(int x, int y)
     {
-        SMove rotate = new SMoveRotate(new List<Vector2Int> {
+        List<Vector2Int> SMoveRotateArr = new List<Vector2Int> { 
                 new Vector2Int(x, y),
                 new Vector2Int(x, y + 1),
                 new Vector2Int(x + 1, y + 1),
                 new Vector2Int(x + 1, y),
-            });
+            };
+
+        for (int i=3; i>=0; i--)
+        {
+            int curX = SMoveRotateArr[i].x;
+            int curY = SMoveRotateArr[i].y;
+
+            STile[,] grid = SGrid.current.GetGrid();
+
+            if (grid[curX, curY].hasAnchor)
+            {
+                SMoveRotateArr.RemoveAt(i);
+            }
+        }
+
+        SMove rotate = new SMoveRotate(SMoveRotateArr);
         // todo: if can rotate
         SGrid.current.Move(rotate);
 
