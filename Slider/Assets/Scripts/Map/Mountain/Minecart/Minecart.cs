0ed38dd44bedb3d244b09fceb1879d03c8641663
ChaseOB <chaseobrien128@gmail.com>
2022-02-12 23:44:54 -0500

    minecart groundwork

--- Slider/Assets/Scripts/Map/Mountain/Minecart/Minecart.cs
+++ Slider/Assets/Scripts/Map/Mountain/Minecart/Minecart.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Minecart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private int currentDirection;
+    [SerializeField] private bool isOnTrack;
 }
--- Slider/Assets/Scripts/Map/Mountain/Minecart/RailTile.cs
+++ Slider/Assets/Scripts/Map/Mountain/Minecart/RailTile.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Tilemaps;
 
-public class RailTile : MonoBehaviour
+public class RailTile : Tile
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private bool state {get; set;}
+    /*Represents connections between points. ENWS = 0123. -1 is used for an unreachable direction. The first index represents the state
+      IE [[1,3,-1,1][1,0,-1,1]] means that E->N, N->S, and S->N in state 0 and E->N, N->E, and S->N in stage 1   
+    */
+    [SerializeField] private int[,] indexMap = new int[2,4];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    [SerializeField] private RailTile[] adjTiles;
+
 }
