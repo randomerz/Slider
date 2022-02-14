6730d1ea8b90f80f37b7e8b342f8d67994944405
Daniel Carr <randomerz56@gmail.com>
2022-02-14 02:19:58 -0500

    code updates

--- Slider/Assets/Scripts/Sliders/Grid/SGridBackground.cs
+++ Slider/Assets/Scripts/Sliders/Grid/SGridBackground.cs
@@ -7,20 +7,20 @@ public class SGridBackground : MonoBehaviour
 {
     // these are the world borders that don't move and prevent you from jumping onto a tile
     public GameObject[] borderColliders; // right top left bottom
-    private int[] BorderCounts = new int[4];
+    private int[] borderCounts = new int[4];
 
     public void SetBorderCollider(int index, bool isActive)
     {
         if (isActive)
-            BorderCounts[index] += 1;
+            borderCounts[index] += 1;
         else
-            BorderCounts[index] -= 1;
-        borderColliders[index].SetActive(BorderCounts[index] > 0);
+            borderCounts[index] -= 1;
+        borderColliders[index].SetActive(borderCounts[index] > 0);
     }
 
     public void SetBorderColliders(bool isActive)
     {
-        for (int i = 0; i < BorderCounts.Length; i++) {
+        for (int i = 0; i < borderCounts.Length; i++) {
             SetBorderCollider(i, isActive);
         }
     }
