c91f53b7e5ec2385120496cd4c19f322b627366e
travis-bittel <tbittel6@gatech.edu>
2022-03-16 12:58:10 -0400

    Add DiscordManager Component to GameManagers

--- Slider/Assets/Scripts/Discord/DiscordController.cs
+++ Slider/Assets/Scripts/Discord/DiscordController.cs
@@ -3,13 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Handles Discord Rich Presence. Should probably be attached to GameManager or
+/// some other object which exists at game start and persists through scenes.
+/// </summary>
 public class DiscordController : MonoBehaviour
 {
     private const long CLIENT_ID = 953335446056882186;
     private Discord.Discord discord; // This looks hilarious but it's how the SDK works
     private long secondsSinceEpoch; // Used for tracking time elapsed
 
-    // Start is called before the first frame update
     void Start()
     {
         // Going with not requiring Discord seems like the safer option to me.
@@ -22,38 +25,31 @@ public class DiscordController : MonoBehaviour
         secondsSinceEpoch = (int)t.TotalSeconds;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Not entirely sure what this does, but apparently it's important
         discord.RunCallbacks();
     }
 
+    /// <summary>
+    /// We call this once every 5s using InvokeRepeating to update our rich presence status.
+    /// </summary>
     void UpdateActivity()
     {
         var activityManager = discord.GetActivityManager();
 
         var activity = new Discord.Activity
         {
-            // Remember to adjust this for mountain and any other weirdChamp areas
             State = $"{SGrid.current.MyArea.GetDisplayName()} ({SGrid.current.GetNumTilesCollected()} / {SGrid.current.GetTotalNumTiles()})",
             Timestamps =
-              {
-                  Start = secondsSinceEpoch
-              },
+            {
+                // You give Discord an Epoch time in seconds and it displays the time elapsed since then
+                Start = secondsSinceEpoch
+            },
             Instance = true,
         };
 
-        activityManager.UpdateActivity(activity, (result) =>
-        {
-            if (result == Discord.Result.Ok)
-            {
-                Debug.Log("Success!");
-            }
-            else
-            {
-                Debug.Log("Failed");
-            }
-        });
+        activityManager.UpdateActivity(activity, (result) => { });
     }
 
     private void OnApplicationQuit()
