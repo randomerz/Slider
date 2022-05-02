198b8ff32abeb82272658dd19b3c9340e4dbf995
Bowers-L <loganmbowers@gmail.com>
2022-05-02 17:06:18 -0400

    Debug Console 'Activate Tiles' also puts the tiles in the correct place now

--- Slider/Assets/Scripts/AI/Pathfinding/STileNavAgent.cs
+++ Slider/Assets/Scripts/AI/Pathfinding/STileNavAgent.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-//Attach this to the agent you want to use STileNavigation
+//Attach this to the agent you want to use STileNavigation (or WorldNavigation)
 public class STileNavAgent : MonoBehaviour
 {
     [HideInInspector]
