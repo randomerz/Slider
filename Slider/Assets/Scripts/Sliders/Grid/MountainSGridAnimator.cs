96e9fafb5524669e5a25755b889fccb7948e5a9e
ChaseOB <chaseobrien128@gmail.com>
2022-05-09 16:09:44 -0400

    small cleanup

--- Slider/Assets/Scripts/Sliders/Grid/MountainSGridAnimator.cs
+++ Slider/Assets/Scripts/Sliders/Grid/MountainSGridAnimator.cs
@@ -82,63 +82,6 @@ public class MountainSGridAnimator : SGridAnimator
         base.InvokeOnStileMoveEnd(stile, moveCoords, move);
     }
 
-    //TODO: Colliders :meownotlikethis:
-    /*
-    private IEnumerator DisableBordersAndColliders(STile[,] grid, SGridBackground[,] bgGrid, HashSet<Vector2Int> positions, Dictionary<Vector2Int, List<int>> borders)
-    {
-        foreach (Vector2Int p in borders.Keys)
-        {
-            if (0 <= p.x && p.x < bgGrid.GetLength(0) && 0 <= p.y && p.y < bgGrid.GetLength(1))
-            {
-                foreach (int i in borders[p])
-                {
-                    bgGrid[p.x, p.y].SetBorderCollider(i, true);
-                }
-            }
-        }
-
-        List<STile> disabledColliders = new List<STile>();
-
-        // if the player is on a slider, disable hitboxes temporarily
-        foreach (Vector2Int p in positions)
-        {
-            // Debug.Log(Player.GetStileUnderneath());
-            if (Player.GetStileUnderneath() != null && Player.GetStileUnderneath().islandId != grid[p.x, p.y].islandId)
-            {
-                // Debug.Log("disabling" +  p.x + " " + p.y);
-                grid[p.x, p.y].SetSliderCollider(false);
-                disabledColliders.Add(grid[p.x, p.y]);
-            }
-        }
-
-        yield return new WaitForSeconds(movementDuration); // ideally this should be called with an event, not after time
-
-        foreach (Vector2Int p in borders.Keys)
-        {
-            if (0 <= p.x && p.x < bgGrid.GetLength(0) && 0 <= p.y && p.y < bgGrid.GetLength(1))
-            {
-                foreach (int i in borders[p])
-                {
-                    bgGrid[p.x, p.y].SetBorderCollider(i, false);
-                }
-            }
-        }
-
-        foreach (STile t in disabledColliders)
-        {
-            t.SetSliderCollider(true);
-        }
-    }
-
-    private IEnumerator EnableTileBorderColliders(STile stile)
-    {
-        stile.SetBorderColliders(true);
-
-        yield return new WaitForSeconds(movementDuration);
-
-        stile.SetBorderColliders(false);
-    }*/
-
     protected override Vector2 GetMovingDirection(Vector2 start, Vector2 end) // include magnitude?
     {
         return start - end;
