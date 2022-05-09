16284a7a9df9cad19e207444d6d99d0c127c49dd
ChaseOB <chaseobrien128@gmail.com>
2022-05-09 00:17:08 -0400

    added remaining buttons to artifact

--- Slider/Assets/Scripts/UI/Artifact/AreaArtifacts/Mountain/MountainArtifactButton.cs
+++ Slider/Assets/Scripts/UI/Artifact/AreaArtifacts/Mountain/MountainArtifactButton.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class MountainArtifactButton : ArtifactTileButton
 {
-    private Vector3 xOffset = new Vector3(28, -15);
-    private Vector3 yOffset = new Vector3(28, 15);
+    private Vector3 xOffset = new Vector3(28, -14);
+    private Vector3 yOffset = new Vector3(28, 14);
     private Vector3 zOffset = new Vector3(0, 59);
     public Vector3 button1Position;
 
