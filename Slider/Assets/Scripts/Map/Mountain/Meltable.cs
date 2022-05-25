82aee24ec6b8ad28d6c77620a523e815c5ea3b20
ChaseOB <chaseobrien128@gmail.com>
2022-05-25 18:11:53 -0400

    refreezing + temp ice sprite

--- Slider/Assets/Scripts/Map/Mountain/Meltable.cs
+++ Slider/Assets/Scripts/Map/Mountain/Meltable.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Meltable : MonoBehaviour
 {
+    public Sprite frozenSprite;
+    public Sprite meltedSprite;
+
+    public SpriteRenderer spriteRenderer;
+
+    public bool isFrozen = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +20,22 @@ public class Meltable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(this.transform.position.y > 62 && !isFrozen)
+            Freeze();
     }
 
     public void Melt()
     {
         Debug.Log("melting");
+        spriteRenderer.sprite = meltedSprite;
+        isFrozen = false;
     }
+
+    public void Freeze()
+    {
+        Debug.Log("freezing");
+        spriteRenderer.sprite = frozenSprite;
+        isFrozen = true;
+    }
+
 }
