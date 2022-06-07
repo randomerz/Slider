8f5a7c7cb3e519b8244e03dd80b8aa13864f0c0d
ChaseOB <chaseobrien128@gmail.com>
2022-06-07 00:09:58 -0400

    new magitech STiles

--- Slider/Assets/Scripts/Sliders/Grid/AreaGrids/MagiTechGrid.cs
+++ Slider/Assets/Scripts/Sliders/Grid/AreaGrids/MagiTechGrid.cs
@@ -6,7 +6,7 @@ public class MagiTechGrid : SGrid
 {
     public static MagiTechGrid instance;
 
-    public int GridOffset = 100; //C: The X distance between the present and past grid
+    public int gridOffset = 100; //C: The X distance between the present and past grid
 
     /* C: The Magitech grid is a 6 by 3 grid. The left 9 STiles represent the present,
     and the right 9 STiles represent the past. The past tile will have an islandID
@@ -56,121 +56,16 @@ public class MagiTechGrid : SGrid
     public override void Load(SaveProfile profile)
     {
         base.Load(profile);
-
-        // Should look into linking this into the save/load system later
-      //  SetAltGrid(altStiles);
-
-        // We want the colliders in the altGrid disabled so they don't push the player around
-    //   StartCoroutine(ISetAltGridCollidersToTrigger());
-        /*foreach (STile tile in altGrid)
-        {
-            tile.SetTileActive(false);
-
-            tile.sliderCollider.isTrigger = true;
-        }*/
-    }
-/*
-    private IEnumerator ISetAltGridCollidersToTrigger()
-    {
-        yield return new WaitForEndOfFrame();
-        foreach (STile tile in altGrid)
-        {
-            tile.SetTileActive(false);
-
-            tile.sliderCollider.isTrigger = true;
-        }
     }
 
-    /*
-     * L: Populates the altGrid[,] array with the given stiles.
-     *
-    private void SetAltGrid(STile[] stiles)
-    {
-        if (stiles.Length != width * height)
-        {
-            Debug.LogError("Only " + stiles.Length + " found when initializing altGrid! " +
-                "Expected " + width * height + ".");
-            return;
-        }
-
-        altGrid = new STile[width, height];
-        foreach (STile t in stiles)
-        {
-            altGrid[t.x, t.y] = t;
-        }
-    }
-
-    public void SwapGrids()
-    {
-        STile[,] temp = grid;
-        grid = altGrid;
-        altGrid = temp;
-
-        foreach (STile tile in altGrid)
-        {
-            tile.SetTileActive(false);
-            tile.sliderCollider.isTrigger = true;
-        }
-        foreach (STile tile in grid)
-        {
-            // We only enable tiles in the current grid if they have been collected already
-            tile.SetTileActive(tile.isTileCollected);
-        }
-    }
-*/
-    // See STile.isTileCollected for an explanation
     public override void CollectSTile(int islandId)
     {
         foreach (STile s in grid)
         {
-            //Debug.Log(s.islandId);
             if (s.islandId == islandId || s.islandId - 9 == islandId)
             {
                 CollectStile(s);
-                //break;
             }
-        }
-        /*foreach (STile s in altGrid)
-        {
-            if (s.islandId == islandId)
-            {
-                CollectStile(s);
-                break;
-            }
-        }*/
-    }/*
-
-    public bool AltGridCanMove(SMove move)
-    {
-        foreach (Movement m in move.moves)
-        {
-            if (!altGrid[m.startLoc.x, m.startLoc.y].CanMove(m.startLoc.x, m.startLoc.y))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-/*
-    public override void Move(SMove move)
-    {
-        base.Move(move);
-
-        // Move the altGrid to match
-        if (AltGridCanMove(move))
-        {
-            gridAnimator.Move(move, altGrid);
-
-            STile[,] newAltGrid = new STile[width, height];
-            System.Array.Copy(altGrid, newAltGrid, width * height);
-            foreach (Movement m in move.moves)
-            {
-                //grid[m.x, m.y].SetGridPosition(m.z, m.w);
-                newAltGrid[m.endLoc.x, m.endLoc.y] = altGrid[m.startLoc.x, m.startLoc.y];
-                //Debug.Log("Setting " + m.x + " " + m.y + " to " + m.z + " " + m.w);
-            }
-            altGrid = newAltGrid;
         }
     }
-    */
 }
--- Slider/Assets/Scripts/Sliders/Grid/AreaSTiles/MagitechSTile.cs
+++ Slider/Assets/Scripts/Sliders/Grid/AreaSTiles/MagitechSTile.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class MagitechSTile : STile
 {
-    public bool isAlt = false;
+    public override Vector3 calculatePosition(int x, int y) 
+    {
+        return new Vector3(x/3 * ((MagiTechGrid) SGrid.current).gridOffset + x % 3 * STILE_WIDTH, y * STILE_WIDTH);
+    }
+
+    public override Vector3 calculateMovingPosition(float x, float y) 
+    {
+        Vector3 newPos = STILE_WIDTH * new Vector3(x, y);
+
+        if(x >= 3)
+            newPos += new Vector3(((MagiTechGrid)SGrid.current).gridOffset - 3 * STILE_WIDTH, 0);
+        return newPos;
+    }
 }
