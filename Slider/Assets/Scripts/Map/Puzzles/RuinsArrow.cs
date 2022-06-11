3e1de1ff5e4636fd65de0e5c5257ef3b74ef9cf9
boomo <randomerz56@gmail.com>
2022-06-11 13:15:43 -0400

    ruins puzzle in now

--- Slider/Assets/Scripts/Map/Puzzles/RuinsArrow.cs
+++ Slider/Assets/Scripts/Map/Puzzles/RuinsArrow.cs
@@ -26,12 +26,15 @@ public class RuinsArrow : MonoBehaviour
         bool afterAssembled = AreRuinsAssembled(SGrid.GetGridString(e.grid));
         bool turnOn = beforeAssembled && afterAssembled;
 
-        SetArrowActive(turnOn);
+        SetArrowActive(turnOn, e.oldGrid); // probably doesnt matter which grid
     }
 
     private void UpdateArrowOnEnd(object sender, SGridAnimator.OnTileMoveArgs e) // SGridAnimator
     {
-        SetArrowActive(AreRuinsAssembled(SGrid.GetGridString(SGrid.current.GetGrid())));
+        SetArrowActive(
+            AreRuinsAssembled(SGrid.GetGridString(SGrid.current.GetGrid())), 
+            SGrid.current.GetGrid()
+        );
     }
 
     private bool AreRuinsAssembled(string gridString)
@@ -39,16 +42,77 @@ public class RuinsArrow : MonoBehaviour
         return CheckGrid.contains(gridString, "31..62");
     }
 
-    public void SetArrowActive(bool value)
+    public void SetArrowActive(bool value, STile[,] grid)
     {
         // check if arrow should be on or not
         if (value)
         {
             spriteRenderer.enabled = true;
+            UpdateArrowDirection(grid);
+
+            SGrid.current.ActivateSliderCollectible(7);
         }
         else
         {
             spriteRenderer.enabled = false;
         }
+    }
+
+    private void UpdateArrowDirection(STile[,] grid)
+    {
+        Vector2Int t2 = Vector2Int.zero;
+        Vector2Int t5 = Vector2Int.zero;
+
+        for (int x = 0; x < grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                if (grid[x, y].islandId == 2)
+                    t2 = new Vector2Int(x, y);
+                else if (grid[x, y].islandId == 5)
+                    t5 = new Vector2Int(x, y);
+            }
+        }
+
+        Vector2Int dif = t5 - t2;
+        
+        if (dif.y == 2)
+        {
+            // NNE
+            if (dif.x == 0 || dif.x == -1)
+            {
+                spriteRenderer.sprite = arrows[0];
+            }
+            // NE
+            else if (dif.x == 1 || dif.x == -2)
+            {
+                spriteRenderer.sprite = arrows[1];
+            }
+        }
+        else if (dif.y == 1)
+        {
+            // ENE
+            spriteRenderer.sprite = arrows[2];
+        }
+        else if (dif.y == 0)
+        {
+            // ESE
+            spriteRenderer.sprite = arrows[3];
+        }
+        else if (dif.y == -1)
+        {
+            // SE
+            if (dif.x == 1 || dif.x == -2)
+            {
+                spriteRenderer.sprite = arrows[4];
+            }
+            // SSE
+            else if (dif.x == 0 || dif.x == -1)
+            {
+                spriteRenderer.sprite = arrows[5];
+            }
+        }
+
+        spriteRenderer.flipX = (dif.x < 0);
     }
 }
