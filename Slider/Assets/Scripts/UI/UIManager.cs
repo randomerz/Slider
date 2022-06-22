67299e2f438ba9c45c4c1838c8a64e732a2c1776
ChaseOB <chaseobrien128@gmail.com>
2022-06-22 15:01:27 -0400

    oops

--- Slider/Assets/Scripts/UI/UIManager.cs
+++ Slider/Assets/Scripts/UI/UIManager.cs
@@ -42,10 +42,6 @@ public class UIManager : MonoBehaviour
         bigTextToggle.onValueChanged.AddListener((bool value) => { UpdateBigText(); });
     }
 
-    public static UIManager GetInstance()
-    {
-        return _instance;
-    }
 
     public static void LoadBindings()
     {
