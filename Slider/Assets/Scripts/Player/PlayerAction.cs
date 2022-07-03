ee09cbdd74d77763174c0507995ef6db0bc37381
travis-bittel <tbittel6@gatech.edu>
2022-07-02 20:17:37 -0400

    Add Drop Item Improvements

--- Slider/Assets/Scripts/Player/PlayerAction.cs
+++ Slider/Assets/Scripts/Player/PlayerAction.cs
@@ -55,18 +55,73 @@ public class PlayerAction : MonoBehaviour
         {
 
             pickedItem.gameObject.transform.position = itemPickupLocation.position;
-            itemDropIndicator.transform.position = GetIndicatorLocation();
 
-            bool dropCastHit = DoesDropRaycastIntersect();
+            Vector2 furthestValidDropPosition = GetFurthestValidDropPosition();
 
-            // We can drop if the path is clear
-            canDrop = !dropCastHit;
-            itemDropIndicator.SetActive(!dropCastHit);
+            canDrop = Vector2.Distance(transform.position, furthestValidDropPosition) > 0.5f;
+            itemDropIndicator.SetActive(canDrop);
+            itemDropIndicator.transform.position = furthestValidDropPosition;
         }
         else if (pickedItem == null)
         {
             itemDropIndicator.SetActive(false);
         }
+    }
+
+    private Vector2 GetFurthestValidDropPosition()
+    {
+        Vector3 raycastDirection = GetIndicatorLocation() - transform.position;
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, 0.5f, raycastDirection, 1.5f, layerMask:dropCollidingMask);
+
+        Vector2 furthestPossibleDropPosition = transform.position;
+        bool hitCollider = false;
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.gameObject != pickedItem.gameObject && !hit.collider.isTrigger)
+            {
+                hitCollider = true;
+
+                // Hit an obstacle
+                if (Vector2.Distance(hit.centroid, transform.position) > Vector2.Distance(furthestPossibleDropPosition, transform.position))
+                {
+                    furthestPossibleDropPosition = hit.centroid;
+                }
+            }
+
+            STile stile = hit.collider.gameObject.GetComponent<STile>();
+            if (stile != null && !stile.isTileActive)
+            {
+                hitCollider = false;
+
+                // Hit an inactive stile
+                if (Vector2.Distance(hit.centroid, transform.position) > Vector2.Distance(furthestPossibleDropPosition, transform.position))
+                {
+                    furthestPossibleDropPosition = hit.centroid;
+                }
+            }
+        }
+
+        if (furthestPossibleDropPosition == (Vector2) transform.position)
+        {
+            if (hitCollider)
+            {
+                // There was a collision, but it wasn't a valid drop location
+                furthestPossibleDropPosition = transform.position;
+            } 
+            else
+            {
+                // Use furthest location if there is no collision
+                Vector3 moveDir = Player.GetLastMoveDir().normalized;
+                if (moveDir.x != 0 && moveDir.y != 0)
+                {
+                    moveDir = moveDir * 0.75f * Mathf.Sqrt(2);
+                }
+                furthestPossibleDropPosition = transform.position + moveDir;
+                Debug.Log("MAX DISTANCE");
+            }
+        }
+        return furthestPossibleDropPosition;
     }
 
     private bool DoesDropRaycastIntersect()
@@ -166,7 +221,7 @@ public class PlayerAction : MonoBehaviour
             {
                 isDropping = true;
                 PlayerInventory.RemoveItem();
-                pickedItem.DropItem(GetIndicatorLocation(), callback:FinishDropping);
+                pickedItem.DropItem(itemDropIndicator.transform.position, callback:FinishDropping);
                 lastDroppedItem = pickedItem;
                 pickedItem = null;
                 itemDropIndicator.SetActive(false);
