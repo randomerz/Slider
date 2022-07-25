16ef1e4eef0e7b83526e36597d3cd9ea9632fc8a
Bowers-L <loganmbowers@gmail.com>
2022-07-25 13:16:53 -0400

    Factory Blackout Shader

--- Slider/Assets/Scripts/Map/Factory/PowerCrystal.cs
+++ Slider/Assets/Scripts/Map/Factory/PowerCrystal.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PowerCrystal : MonoBehaviour
 {
+    private const string lightShaderName = "Shader Graphs/LightShaderBoolean";
+
     private ElectricalNode[] allNodes;
     private Conveyor[] conveyors;
+    private List<Material> lightMaterials;
 
     public static bool Blackout { get; private set; }
 
@@ -14,6 +17,8 @@ public class PowerCrystal : MonoBehaviour
         Blackout = false;
         allNodes = GameObject.FindObjectsOfType<ElectricalNode>();
         conveyors = GameObject.FindObjectsOfType<Conveyor>();
+
+        lightMaterials = FindMaterials();
     }
 
     public void StartCrystalPoweredSequence()
@@ -44,5 +49,28 @@ public class PowerCrystal : MonoBehaviour
         {
             conveyor.ConveyorEnabled = false;
         }
+
+        foreach (var mat in lightMaterials)
+        {
+            mat.SetInt("_LightOn", 0);  //L: No set bool? Ya serious?
+        }
+    }
+
+    private List<Material> FindMaterials()
+    {
+        List<Material> materials = new List<Material>();
+        Shader lightShader = Shader.Find(lightShaderName);
+
+        Renderer[] allRenderers = FindObjectsOfType<Renderer>(true);
+        foreach (Renderer r in allRenderers)
+        {
+            if (r.material.shader == lightShader)
+            {
+                materials.Add(r.material);
+                //Debug.Log(r.gameObject.name);
+            }
+        }
+
+        return materials;
     }
 }
