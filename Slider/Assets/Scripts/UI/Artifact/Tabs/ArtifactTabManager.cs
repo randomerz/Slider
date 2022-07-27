ac7e5014605969c2bcff613b310fd9cf6681239e
Elliott Chen <37708746+Elioc997472@users.noreply.github.com>
2022-07-27 02:38:49 -0600

    Fixing Frag Tab
    Cleaned up some outdated comments and bandaid fix for frag realign

--- Slider/Assets/Scripts/UI/Artifact/Tabs/ArtifactTabManager.cs
+++ Slider/Assets/Scripts/UI/Artifact/Tabs/ArtifactTabManager.cs
@@ -240,9 +240,10 @@ public class ArtifactTabManager : MonoBehaviour
             AudioManager.Play("Artifact Error");
             return;
         }
-        uiArtifactMenus.uiArtifact.TryQueueMoveFromButtonPair(middle, empty);
-        uiArtifactMenus.uiArtifact.UpdatePushedDowns(null, null);
-        uiArtifactMenus.uiArtifact.DeselectSelectedButton();
+        DesertArtifact artifact = (DesertArtifact) uiArtifactMenus.uiArtifact;
+        artifact.TryFragQueueMoveFromButtonPair(middle, empty);
+        artifact.UpdatePushedDowns(null, null);
+        artifact.DeselectSelectedButton();
         //FragRearrangeOnHoverExit();
     }
 
@@ -255,8 +256,6 @@ public class ArtifactTabManager : MonoBehaviour
         if (middle.TileIsActive) UIArtifact.SetLightningPos(1, 1);
         middle.SetLightning(true);
         empty.SetLightning(true);        
-        //middle.FragLightningPreview(true);
-        //empty.FragLightningPreview(true);
     }
 
     public void FragRearrangeOnHoverExit()
@@ -266,8 +265,6 @@ public class ArtifactTabManager : MonoBehaviour
         UIArtifact.DisableLightning(true);
         middle.SetLightning(false);
         empty.SetLightning(false);
-        //middle.FragLightningPreview(false);
-        //empty.FragLightningPreview(false);
     }
 
     //Preview Tab
