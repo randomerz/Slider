322efe589abb004c09904c05030a98fa7916b17c
Allison Lu <allison.lu@gatech.edu>
2022-08-03 16:11:47 -0400

    22 shapes

--- Slider/Assets/Scripts/Map/Jungle/Shape.cs
+++ Slider/Assets/Scripts/Map/Jungle/Shape.cs
@@ -2,19 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "Shape", menuName = "Scriptable Objects/Shape")]
 public class Shape : ScriptableObject
 {
-    string name;
-    Sprite sprite;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string name;
+    public Sprite sprite;
 }
