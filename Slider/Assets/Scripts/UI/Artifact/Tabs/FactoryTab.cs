ac154346c82e4d70caa20964364f5f1f74ee1c8c
Elliott Chen <37708746+Elioc997472@users.noreply.github.com>
2022-08-04 00:16:17 -0600

    Update FactoryTab.cs
    Fix Diodes turning off after you get off them (more editor scuff)
    Support for multiple factoryTabs. Have no clue why inserting the if statement before the sprite assignment fixes some null-reference but okay I guess.

--- Slider/Assets/Scripts/UI/Artifact/Tabs/FactoryTab.cs
+++ Slider/Assets/Scripts/UI/Artifact/Tabs/FactoryTab.cs
@@ -20,6 +20,7 @@ public class FactoryTab : ArtifactTab
     private void OnEnable()
     {
         UIArtifact.MoveMadeOnArtifact += MoveMadeOnArtifact;
+        if (gate.Countdown <= 0) gameObject.SetActive(false);
         queuedNextSprite = countdownSprite[gate.Countdown];
     }
 
@@ -51,6 +52,7 @@ public class FactoryTab : ArtifactTab
 
     private void MoveMadeOnArtifact(object sender, System.EventArgs e)
     {
+        Debug.Log("MoveMadeOnArtifact");
         if (gate.GateActive && !gate.Powered)
         {
             if (gate.Countdown > 0)
@@ -61,24 +63,14 @@ public class FactoryTab : ArtifactTab
             else if (gate.Countdown == 0)
             {
                 queuedNextSprite = countdownSprite[gate.Countdown]; //set to 0
-                                                                    //StartCoroutine(BlinkUntilNextSpriteChange());
-                                                                    //waitingToEndGate = StartCoroutine(WaitAfterMove(EvaluateGate));
-                queuedNextSprite = gate.Powered ? successSprite : failureSprite;
+                StartCoroutine(BlinkUntilNextSpriteChange());
+                StartCoroutine(WaitAfterMove());
             }
             else if (gate.Countdown < 0)
             {
-                //If player tries to queue another move, just stop the gate immediately. (avoids some nasty edge cases)
-                /*
-                if (waitingToEndGate != null)
-                {
-                    StopCoroutine(waitingToEndGate);
-                }
-
-                EvaluateGate();
-                */
+                StopAllCoroutines();
                 SetIsVisible(false);
             }
-            image.sprite = queuedNextSprite;
         }
     }
 
@@ -86,23 +78,72 @@ public class FactoryTab : ArtifactTab
     {
         if (!blinking)
         {
-            Debug.Log("Entered Blink!");
             blinking = true;
             int currBlinks = numBlinks;
             while (currBlinks > 0)
             {
+                Debug.Log("num blinks: " + currBlinks);
                 image.sprite = blinkSprite;
-                yield return new WaitForSeconds(0.25f);
+                yield return new WaitForSeconds(0.5f);
                 image.sprite = queuedNextSprite;
                 currBlinks--;
                 if (currBlinks > 0)
                 {
-                    yield return new WaitForSeconds(0.25f);
+                    yield return new WaitForSeconds(0.5f);
                 }
             }
 
             image.sprite = queuedNextSprite;
             blinking = false;
         }
+    }
+    private IEnumerator WaitAfterMove()
+    {
+        bool tilesAreMoving = true;
+        while (tilesAreMoving)
+        {
+            tilesAreMoving = SGrid.Current.TilesMoving();
+
+            if (!tilesAreMoving)
+            {
+                //Wait a bit, and then check again. If there's still no movement, then we can safely turn off the gate.
+                //This gives the player enough leeway to complete the puzzle at the last second (i.e. puzzle 3c)
+                yield return new WaitForSeconds(0.4f);
+
+                tilesAreMoving = SGrid.Current.TilesMoving();
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+        queuedNextSprite = gate.Powered ? successSprite : failureSprite;
+        StartCoroutine(WaitBeforeDisabling());
+    }
+    private IEnumerator BlinkUntilNextSpriteChange()
+    {
+        if (!blinking)
+        {
+            blinking = true;
+            Sprite currSprite = queuedNextSprite;
+            while (queuedNextSprite == currSprite)
+            {
+                image.sprite = blinkSprite;
+                yield return new WaitForSeconds(0.25f);
+                image.sprite = queuedNextSprite;
+                if (currSprite == queuedNextSprite)
+                {
+                    yield return new WaitForSeconds(0.25f);
+                }
+            }
+            image.sprite = queuedNextSprite;
+            blinking = false;
+        }
+    }
+
+    private IEnumerator WaitBeforeDisabling()
+    {
+        yield return new WaitForSeconds(1f);
+        SetIsVisible(false);
     }
 }
