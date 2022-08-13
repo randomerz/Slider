8d024768db1bbad7e579740d8721075fb0fcc4c5
wennethkang <kennethzwang@gmail.com>
2022-08-13 18:33:57 -0400

    laser w/ portals

--- Slider/Assets/Scripts/Map/Puzzles/MagiLaser.cs
+++ Slider/Assets/Scripts/Map/Puzzles/MagiLaser.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class MagiLaser : MonoBehaviour
 {
     public Vector2 initDir;
-    private LineRenderer lineRenderer;
+
     private RaycastHit2D hit;
-    private Vector2 curDir;
-    private Vector2 curPos;
+    private LineRenderer lineRenderer, lineRenderer2, lineRenderer3;
+    private Vector2 curDir, curDir2, curDir3;
+    private Vector2 curPos, curPos2, curPos3;
     private void Awake() {
-        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer = transform.GetChild(0).GetComponent<LineRenderer>();
+        lineRenderer2 = transform.GetChild(1).GetComponent<LineRenderer>();
+        lineRenderer3 = transform.GetChild(2).GetComponent<LineRenderer>();
     }
     void Update()
     {
@@ -30,23 +33,56 @@ public class MagiLaser : MonoBehaviour
         bool incomplete = true;
         
         while(incomplete) {
-            hit = Physics2D.Raycast(curPos, curDir, 34.0f, 4096);
+            hit = Physics2D.Raycast(curPos, curDir, 40.0f, 4096);
             if(hit){
                 lineRenderer.positionCount += 1;
                 lineRenderer.SetPosition(lineRenderer.positionCount-1, hit.collider.transform.position);
                 if(hit.collider.tag == "Mirror1") {
                     curDir = MirrorOneReflect(curDir);
                     curPos = hit.collider.transform.position + (Vector3)(curDir * 1.1f);
+                    lineRenderer2.positionCount = 1;
                 } else if(hit.collider.tag == "Mirror2") {
                     curDir = MirrorTwoReflect(curDir);
                     curPos = hit.collider.transform.position + (Vector3)(curDir * 1.1f);
-                } else if(hit.collider.tag == "Portal") {
-                    
+                    lineRenderer2.positionCount = 1;
+                } else if(hit.collider.tag == "Portal1") {
+                    MakeLaser2(curDir);
                     incomplete = false;
                 }
             } else {
                 lineRenderer.positionCount += 1;
                 lineRenderer.SetPosition(lineRenderer.positionCount-1, curPos + 34.0f*curDir);
+                lineRenderer2.positionCount = 1;
+                incomplete = false;
+            }
+        }
+    }
+    private void MakeLaser2(Vector2 initDir2)
+    {
+        lineRenderer2.positionCount = 1;
+        Vector3 initPos2 = GameObject.FindWithTag("Portal2").transform.position + (Vector3)(initDir2*0.8f);
+        lineRenderer2.SetPosition(0,initPos2);
+        curDir2 = initDir2;
+        curPos2 = initPos2;
+        bool incomplete = true;
+        
+        while(incomplete) {
+            hit = Physics2D.Raycast(curPos2, curDir2, 40.0f, 4096);
+            if(hit){
+                lineRenderer2.positionCount += 1;
+                lineRenderer2.SetPosition(lineRenderer2.positionCount-1, hit.collider.transform.position);
+                if(hit.collider.tag == "Mirror1") {
+                    curDir2 = MirrorOneReflect(curDir2);
+                    curPos2 = hit.collider.transform.position + (Vector3)(curDir2 * 1.1f);
+                } else if(hit.collider.tag == "Mirror2") {
+                    curDir2 = MirrorTwoReflect(curDir2);
+                    curPos2 = hit.collider.transform.position + (Vector3)(curDir2 * 1.1f);
+                } else if(hit.collider.tag == "Portal2") {
+                    incomplete = false;
+                }
+            } else {
+                lineRenderer2.positionCount += 1;
+                lineRenderer2.SetPosition(lineRenderer2.positionCount-1, curPos2 + 34.0f*curDir2);
                 incomplete = false;
             }
         }
