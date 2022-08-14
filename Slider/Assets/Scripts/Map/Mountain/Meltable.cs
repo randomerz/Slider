2741cde2fbb16c43cd883503bff85fd7fe09979e
ChaseOB <chaseobrien128@gmail.com>
2022-08-14 12:57:52 -0400

    meltable changes

--- Slider/Assets/Scripts/Map/Mountain/Meltable.cs
+++ Slider/Assets/Scripts/Map/Mountain/Meltable.cs
@@ -6,27 +6,40 @@ using UnityEngine.Events;
 
 public class Meltable : MonoBehaviour
 {
-    public Sprite frozenSprite; //C: switch to animation later maybe? but that doesn't sound fun
+    [Header("Sprites")]
+   // [SerializeField] private Animator animator; C: Will switch over later
+    
+    public Sprite frozenSprite;
     public Sprite meltedSprite;
     public Sprite anchorBrokenSprite;
     public SpriteRenderer spriteRenderer;
 
     public bool isFrozen = true;
-  //  private bool canBeChanged = true;
 
+    [Header("Events")]
     public UnityEvent onMelt;
     public UnityEvent onFreeze;
+    public UnityEvent onBreak;
     public UnityEvent onFix;
+
+
+    [Header("Properties")]
+    [SerializeField] private  bool canBreakWithAnchor = true;
+    [SerializeField] private  bool canBreakWhenNotFrozen = false;
+    [SerializeField] private  bool refreezeOnTop = true;
+    [SerializeField] private  bool refreezeFromBroken = false;
+    [SerializeField] private bool fixBackToFrozen = false;
+
 
     public int numLavaSources = 0;
     public bool anchorBroken = false;
-    public bool canBreakWithAnchor = true;
     private int numTimesBroken = 0;
-    public STile sTile;
+    private STile sTile;
 
     private void OnEnable() {
-        MountainSGridAnimator.OnSTileMoveEnd += CheckFreezeOnMoveEnd;
         sTile = GetComponentInParent<MountainSTile>();
+        Debug.Log(gameObject.name + " " + sTile.islandId);
+        MountainSGridAnimator.OnSTileMoveEnd += CheckFreezeOnMoveEnd;
     }
 
     private void OnDisable() {
@@ -38,37 +51,68 @@ public class Meltable : MonoBehaviour
         if(sTile == null || e.stile == null)
             return;
         if(sTile != null && e.stile.islandId == sTile.islandId)
-            Debug.Log("amogus");
-        if(sTile != null && e.stile.islandId == sTile.islandId && sTile.y > 1 && !isFrozen && numLavaSources <= 0 && !anchorBroken)
-            Freeze();
+        {
+            Debug.Log("same stile " + gameObject.name);
+            if(CheckFreeze())
+                Freeze();
+        }
     }
 
-    public virtual void Melt(bool usingAnchor = false)
+    public bool CheckFreeze()
     {
-        Debug.Log("melt");
-        if(usingAnchor && canBreakWithAnchor && !anchorBroken)
+        return (!refreezeOnTop || sTile.y > 1) && (!anchorBroken || refreezeFromBroken) && numLavaSources <= 0;
+    }
+
+    public void Break() {
+        if((isFrozen || canBreakWhenNotFrozen) && canBreakWithAnchor && !anchorBroken)
         {
             anchorBroken = true;
             numTimesBroken++;
-        }
-        if(isFrozen && (anchorBroken || numLavaSources > 0)) //C: the second check is pointless but maybe wacky things could happen
-        {
-            if (spriteRenderer)
-            {
-                if(anchorBrokenSprite != null && anchorBroken)
-                    spriteRenderer.sprite = anchorBrokenSprite;
-                else
-                    spriteRenderer.sprite = meltedSprite;
-            }
             isFrozen = false;
-            onMelt.Invoke();
+           // animator.SetBool("Broken", true);
+            if(spriteRenderer)
+                spriteRenderer.sprite = anchorBrokenSprite;
+            onBreak.Invoke();
         }
     }
 
-   // public void SetCanBeChanged(bool value)
-  //  {
-   //     canBeChanged = value;
-  //  }
+    public void Melt()
+    {
+        if(isFrozen && numLavaSources > 0) 
+        {
+            isFrozen = false;
+            if(spriteRenderer)
+                spriteRenderer.sprite = meltedSprite;
+            onMelt.Invoke();
+            Debug.Log("Melt "+ gameObject.name);
+        }
+    }
+
+    public void Freeze()
+    {
+        if(!isFrozen)
+        {
+            isFrozen = true;
+            if(spriteRenderer)
+                spriteRenderer.sprite = frozenSprite;
+            onFreeze.Invoke();
+            Debug.Log("Freeze " + gameObject.name);
+        }
+    }
+
+    public void Fix()
+    {
+        if(anchorBroken) 
+        {
+            anchorBroken = false;
+           // animator.SetBool("Broken", false);
+            if(fixBackToFrozen)
+                Freeze();
+            else if(spriteRenderer)
+                spriteRenderer.sprite = frozenSprite;
+            onFix.Invoke();
+        }
+    }
 
     public void RemoveLava()
     {
@@ -79,11 +123,6 @@ public class Meltable : MonoBehaviour
     {
         numLavaSources++;
         Melt();
-    }
-
-    public void RemoveAnchor()
-    {
-        anchorBroken = false;
     }
 
     public void IsFrozen(Condition c) {
@@ -104,22 +143,5 @@ public class Meltable : MonoBehaviour
 
     public void HasBeenBrokenMultipleTimes(Condition c){
         c.SetSpec(numTimesBroken > 1);
-    }
-
-    public void Freeze()
-    {
-        Debug.Log("Freeze");
-        if (spriteRenderer)
-            spriteRenderer.sprite = frozenSprite;
-        isFrozen = true;
-        onFreeze.Invoke();
-    }
-
-    public void Fix()
-    {
-        anchorBroken = false;
-        if (spriteRenderer)
-            spriteRenderer.sprite = meltedSprite;
-        onFix.Invoke();
     }
 }
