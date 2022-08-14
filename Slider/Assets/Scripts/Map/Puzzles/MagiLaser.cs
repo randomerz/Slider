336a2a744268e7e24d6578a4f56dce5aae806eb6
wennethkang <kennethzwang@gmail.com>
2022-08-14 01:28:36 -0400

    magitech laser done pt1

--- Slider/Assets/Scripts/Map/Puzzles/MagiLaser.cs
+++ Slider/Assets/Scripts/Map/Puzzles/MagiLaser.cs
@@ -25,64 +25,50 @@ public class MagiLaser : MonoBehaviour
     }
     private void MakeLaser()
     {
-        lineRenderer.positionCount = 1;
         Vector3 initPos = transform.position + new Vector3(-3.0f,1.0f,0.0f);
-        lineRenderer.SetPosition(0,initPos);
         curDir = initDir;
         curPos = initPos;
-        bool incomplete = true;
-        
-        while(incomplete) {
-            hit = Physics2D.Raycast(curPos, curDir, 40.0f, 4096);
-            if(hit){
-                lineRenderer.positionCount += 1;
-                lineRenderer.SetPosition(lineRenderer.positionCount-1, hit.collider.transform.position);
-                if(hit.collider.tag == "Mirror1") {
-                    curDir = MirrorOneReflect(curDir);
-                    curPos = hit.collider.transform.position + (Vector3)(curDir * 1.1f);
-                    lineRenderer2.positionCount = 1;
-                } else if(hit.collider.tag == "Mirror2") {
-                    curDir = MirrorTwoReflect(curDir);
-                    curPos = hit.collider.transform.position + (Vector3)(curDir * 1.1f);
-                    lineRenderer2.positionCount = 1;
-                } else if(hit.collider.tag == "Portal1") {
-                    MakeLaser2(curDir);
-                    incomplete = false;
-                }
-            } else {
-                lineRenderer.positionCount += 1;
-                lineRenderer.SetPosition(lineRenderer.positionCount-1, curPos + 34.0f*curDir);
-                lineRenderer2.positionCount = 1;
-                incomplete = false;
-            }
-        }
+        DrawLaser(curDir, curPos, lineRenderer);
     }
     private void MakeLaser2(Vector2 initDir2)
     {
-        lineRenderer2.positionCount = 1;
+        
         Vector3 initPos2 = GameObject.FindWithTag("Portal2").transform.position + (Vector3)(initDir2*0.8f);
-        lineRenderer2.SetPosition(0,initPos2);
+        
         curDir2 = initDir2;
         curPos2 = initPos2;
+
+        DrawLaser(curDir2, curPos2, lineRenderer2);
+    }
+
+    private void DrawLaser(Vector2 dir, Vector2 pos, LineRenderer lr) 
+    {
+        lr.positionCount = 1;
+        lr.SetPosition(0,pos);
         bool incomplete = true;
         
         while(incomplete) {
-            hit = Physics2D.Raycast(curPos2, curDir2, 40.0f, 4096);
+            hit = Physics2D.Raycast(pos, dir, 40.0f, 4096);
             if(hit){
-                lineRenderer2.positionCount += 1;
-                lineRenderer2.SetPosition(lineRenderer2.positionCount-1, hit.collider.transform.position);
+                lr.positionCount += 1;
+                lr.SetPosition(lr.positionCount-1, hit.collider.transform.position);
                 if(hit.collider.tag == "Mirror1") {
-                    curDir2 = MirrorOneReflect(curDir2);
-                    curPos2 = hit.collider.transform.position + (Vector3)(curDir2 * 1.1f);
+                    dir = MirrorOneReflect(dir);
+                    pos = hit.collider.transform.position + (Vector3)(dir * 1.1f);
                 } else if(hit.collider.tag == "Mirror2") {
-                    curDir2 = MirrorTwoReflect(curDir2);
-                    curPos2 = hit.collider.transform.position + (Vector3)(curDir2 * 1.1f);
+                    dir = MirrorTwoReflect(dir);
+                    pos = hit.collider.transform.position + (Vector3)(dir * 1.1f);
+                } else if(hit.collider.tag == "Portal1") {
+                    MakeLaser2(dir);
+                    incomplete = false;
                 } else if(hit.collider.tag == "Portal2") {
+                    incomplete = false;
+                } else {
                     incomplete = false;
                 }
             } else {
-                lineRenderer2.positionCount += 1;
-                lineRenderer2.SetPosition(lineRenderer2.positionCount-1, curPos2 + 34.0f*curDir2);
+                lr.positionCount += 1;
+                lr.SetPosition(lr.positionCount-1, pos + 34.0f*dir);
                 incomplete = false;
             }
         }
