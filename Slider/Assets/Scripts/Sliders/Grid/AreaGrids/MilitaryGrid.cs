bf252272f35bb6b7c15bc9bacb9174fe098195ac
Daniel Carr <randomerz56@gmail.com>
2022-09-26 22:57:42 -0400

    military base grid

---
+++ Slider/Assets/Scripts/Sliders/Grid/AreaGrids/MilitaryGrid.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilitaryGrid : SGrid
+{
+    public override void Init()
+    {
+        InitArea(Area.Military);
+        base.Init();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        AudioManager.PlayMusic("Military");
+        UIEffects.FadeFromBlack();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    public override void Save()
+    {
+        base.Save();
+    }
+
+    public override void Load(SaveProfile profile)
+    {
+        base.Load(profile);
+    }
+
+
+    // === Military puzzle specific ===
+}
---
+++ Slider/Assets/Scripts/UI/Artifact/AreaArtifacts/Military/MilitaryArtifactButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MilitaryArtifactButton : ArtifactTileButton
+{
+    // private Vector3 xOffset = new Vector3(28, -14);
+    // private Vector3 yOffset = new Vector3(28, 14);
+    // private Vector3 zOffset = new Vector3(0, 59);
+    // public Vector3 button1Position;
+
+    protected override void Start()
+    {
+        base.Start();
+        // this.GetComponentInChildren<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+    protected override void SetAnchoredPos(int x, int y)
+    {
+        base.SetAnchoredPos(x, y);
+        // Vector3 pos = button1Position + x * xOffset + (y % 2) * yOffset + (y / 2) * zOffset;
+        // GetComponent<RectTransform>().anchoredPosition = pos;
+    }
+}
