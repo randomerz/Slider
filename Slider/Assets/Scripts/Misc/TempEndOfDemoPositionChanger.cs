8c5349ed4eb05fffc0b069e72df5be8d56da601a
Daniel Carr <randomerz56@gmail.com>
2022-11-16 18:29:31 -0500

    small polish that chase mentioned: raft, dialogue, cave icons, finish game save/load

--- Slider/Assets/Scripts/Misc/TempEndOfDemoPositionChanger.cs
+++ Slider/Assets/Scripts/Misc/TempEndOfDemoPositionChanger.cs
@@ -6,12 +6,36 @@ public class TempEndOfDemoPositionChanger : MonoBehaviour
 {
     // This script is so when the player finishes the demo and relaunches the save, they don't spawn 
     // in the cave transition hitbox.
+
+    public SceneChanger sceneChanger;
+    public Transform nearbySpawn;
+    private bool tpToNearbySpawn = true;
+
     void Start()
     {
-        if (Player.GetPosition().y > transform.position.y)
+        // if (Player.GetPosition().y > transform.position.y)
+        // {
+        //     Player.SetPosition(Player.GetPosition() - 2 * Vector3.up);
+        // }
+        StartCoroutine(TurnOffSpawnProtection());
+    }
+
+    public void DemoChangeScene()
+    {
+        if (tpToNearbySpawn)
         {
-            Player.SetPosition(Player.GetPosition() - 2 * Vector3.up);
+            Player.SetPosition(nearbySpawn.position);
+        }
+        else
+        {
+            sceneChanger.ChangeScenes();
         }
     }
     
+    private IEnumerator TurnOffSpawnProtection()
+    {
+        yield return new WaitForSeconds(0.25f);
+
+        tpToNearbySpawn = false;
+    }
 }
