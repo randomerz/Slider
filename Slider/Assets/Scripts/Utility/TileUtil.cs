5af88521fa076b75988f02f4dea43ed1e97a3585
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2022-11-16 00:39:37 -0500

    Moss Updating Every Frame

--- Slider/Assets/Scripts/Utility/TileUtil.cs
+++ Slider/Assets/Scripts/Utility/TileUtil.cs
@@ -2,10 +2,11 @@
 
 public class TileUtil
 {
+    private const float epsilon = 0.001f;
 
     public static Vector2Int WorldToTileCoords(Vector2 posInWorld)
     {
         //L: Tiles are positioned based on the bottom left corner, so need to add small amount to avoid off by 1 error.
-        return new Vector2Int(Mathf.RoundToInt(posInWorld.x + 0.01f), Mathf.RoundToInt(posInWorld.y + 0.01f));
+        return new Vector2Int(Mathf.RoundToInt(posInWorld.x+epsilon), Mathf.RoundToInt(posInWorld.y+epsilon));
     }
 }
