7716568bff3c6bf0909e928813869c45c1425e6c
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2022-11-20 18:45:27 -0500

    New Factory Buttons (more consistent)

--- Slider/Assets/Scripts/Map/Factory/FactoryButton.cs
+++ Slider/Assets/Scripts/Map/Factory/FactoryButton.cs
@@ -1,52 +1,94 @@
+using ClipperLib;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FactoryButton : ElectricalNode
 {
-    [SerializeField] private Animator animator;
-
+    [Header("Factory Button")]
+    [SerializeField] private bool logTrace = false;
+    private Animator _animator;
+    private Collider2D _buttonCollider;
     private int numObjectsOn;
 
     private new void Awake()
     {
         base.Awake();
         nodeType = NodeType.INPUT;
-        numObjectsOn = 0;
 
-        animator ??= GetComponent<Animator>();
+        _buttonCollider = GetComponent<Collider2D>();
+        _animator = GetComponent<Animator>();
     }
+
+    private void Update()
+    {
+        bool buttonPressed = GetButtonPressed();
+        if (buttonPressed != Powered)
+        {
+            Switch(buttonPressed);
+        }
+    }
+
     public void Switch(bool powered)
     {
         StartSignal(powered);
-        animator.SetBool("Powered", powered);
+        _animator.SetBool("Powered", powered);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private bool GetButtonPressed()
     {
-        //Might have to restrict this to specific items, but it works for now.
-        if (other.CompareTag("Player") || other.CompareTag("ButtonTrigger"))
-        {
-            if (numObjectsOn == 0)
-            {
-                Switch(true);
-            }
-            numObjectsOn++;
-        }
+        int layerMask = LayerMask.GetMask("Player", "ButtonTrigger");
+        var filter = new ContactFilter2D();
+        filter.useTriggers = true;
+        filter.layerMask = layerMask;
+        filter.useLayerMask = true;
 
-    }
+        var contacts = new List<Collider2D>();
+        _buttonCollider.GetContacts(filter, contacts);
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("ButtonTrigger"))
+        foreach (Collider2D other in contacts)
         {
-            numObjectsOn--;
-            if (numObjectsOn <= 0)
+            int otherLayer = 1 << other.gameObject.layer;
+            LogTrace($"Other Layer: {LayerMask.LayerToName(other.gameObject.layer)}");
+            bool correctLayer = (layerMask & otherLayer) != 0;
+            if (correctLayer && CheckStandingOn(other))
             {
-                numObjectsOn = 0;
-                Switch(false);
+                return true;
             }
         }
 
+        return false;
+    }
+
+    private bool CheckStandingOn(Collider2D other)
+    {
+        Vector3 floorPoint;
+        if (other is BoxCollider2D)
+        {
+            BoxCollider2D bc = other as BoxCollider2D;
+            floorPoint = bc.bounds.center + Vector3.down * bc.bounds.extents.y;
+            LogTrace($"Floor Point Box: {floorPoint}");
+        } else if (other is CircleCollider2D)
+        {
+            CircleCollider2D cc = other as CircleCollider2D;
+            floorPoint = cc.bounds.center + Vector3.down * cc.radius;
+            LogTrace($"Floor Point Circle: {floorPoint}");
+        } else
+        {
+            //L: I'm pretty sure we're only using box colliders and circle colliders for items, but otherwise this will still
+            //work
+            floorPoint = other.gameObject.transform.position;
+            LogTrace($"Floor Point Anything: {floorPoint}");
+        }
+
+        return _buttonCollider.OverlapPoint(floorPoint);
+    }
+
+    private void LogTrace(string s)
+    {
+        if (logTrace)
+        {
+            Debug.Log(s);
+        }
     }
 }
