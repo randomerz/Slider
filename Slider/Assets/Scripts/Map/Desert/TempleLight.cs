294f0d40905ee2159a895bb6e4dfb03ff0b3ccf8
ChaseOB <chaseobrien128@gmail.com>
2023-01-01 03:08:07 -0500

    light radius change over time

--- Slider/Assets/Scripts/Map/Desert/TempleLight.cs
+++ Slider/Assets/Scripts/Map/Desert/TempleLight.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class TempleLight : MonoBehaviour
 {
+    private float scale;
+    public float delta = 0.1f;
+    public float flickerSpeed = 0.5f;
+    private float currScale;
+    private float randOffset;
+
+    private void Awake() {
+        scale = transform.localScale.x;
+        randOffset = Random.Range(0, 2 * Mathf.PI);
+    }
+
+    private void Update() {
+        currScale = scale + delta * Mathf.Sin(Time.time * flickerSpeed + randOffset);
+        transform.localScale = new Vector3(currScale, currScale, 1);
+    } 
+    
+    
     private int radius = 2;
     public bool dynamic = false;
 
