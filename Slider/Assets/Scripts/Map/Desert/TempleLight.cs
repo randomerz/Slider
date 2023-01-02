6436d9525f9c906f2c418c5fb324da1edfe6f067
ChaseOB <chaseobrien128@gmail.com>
2023-01-02 15:49:10 -0500

    fix caves light bug

--- Slider/Assets/Scripts/Map/Desert/TempleLight.cs
+++ Slider/Assets/Scripts/Map/Desert/TempleLight.cs
@@ -19,39 +19,4 @@ public class TempleLight : MonoBehaviour
         currScale = scale + delta * Mathf.Sin(Time.time * flickerSpeed + randOffset);
         transform.localScale = new Vector3(currScale, currScale, 1);
     } 
-    
-    
-    private int radius = 2;
-    public bool dynamic = false;
-
-    public void UpdateLightMask(ref Color[] lightMask, int worldToMaskDX, int worldToMaskDY, int maskSizeX, int maskSizeY, bool dynamicOnly)
-    {
-        //if(dynamicOnly && !dynamic){
-         //   print("aborted: not dynamic");
-       // return; }
-        if(dynamic)
-            print("dynamic");
-        Vector2Int lightPos = new Vector2Int((int)transform.position.x, (int)transform.position.y);
-        for(int x = -1 * radius; x <= radius; x++)
-        {
-            for(int y = -1 * radius; y <= radius; y++)
-            {
-                if((x * x) + (y * y) < (radius * radius))
-                {
-                    int maskX = lightPos.x + x + worldToMaskDX;
-                    int maskY = lightPos.y + y + worldToMaskDY;
-                    if (maskX < 0 || maskX >= maskSizeX || maskY < 0 || maskY >= maskSizeY);
-                    else
-                        lightMask[maskY * maskSizeX + maskX] = Color.white;
-
-
-                }
-                else
-                {
-                    int res = (x * x) + (y * y);
-                   // print($"Fail: {x}, {y}. {res}");
-                }
-            }
-        }
-    }
 }
