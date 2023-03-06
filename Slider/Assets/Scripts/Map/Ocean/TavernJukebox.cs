463862ce50638aa9690ca73d4f108f39cef1640d
Daniel Carr <randomerz56@gmail.com>
2023-03-06 16:26:35 -0500

    i tried to add the tempo markers

--- Slider/Assets/Scripts/Map/Ocean/TavernJukebox.cs
+++ Slider/Assets/Scripts/Map/Ocean/TavernJukebox.cs
@@ -6,6 +6,23 @@ public class TavernJukebox : MonoBehaviour, ISavable
 {
     private int currentIndex;
     private const int NUMBER_OF_SONGS = 14;
+
+    private readonly float[] songBPMs = new float[] {
+        120f,   // Tavern
+        85f,    // Ocean
+        85f,    // Ocean Island - its actually 170
+        100f,   // Village
+        95f,    // Caves - i think
+        170f,   // Jungle
+        177f,   // Casino
+        116f,   // Factory - probably wrong T ^ T
+        105f,   // Military
+        166f,   // Mountain - i think
+        80f,    // MagiTech Future
+        80f,    // MagiTech Past
+        150f,   // Menu
+        120f,   // Trailer
+    };
     
     [SerializeField] private NPC npc;
 
@@ -16,7 +33,7 @@ public class TavernJukebox : MonoBehaviour, ISavable
 
     public void Save()
     {
-        SaveSystem.Current.SetInt("oceanTavernJukeboxIndex", NUMBER_OF_SONGS);
+        SaveSystem.Current.SetInt("oceanTavernJukeboxIndex", currentIndex);
     }
 
     public void Load(SaveProfile profile)
