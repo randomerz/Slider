3cc04b71be11f2376c3c4131cfec5b23cf0d3312
Daniel Carr <randomerz56@gmail.com>
2023-04-14 00:10:22 -0400

    chad dice puzzle updated

--- Slider/Assets/Scripts/Map/Desert/CasinoDiscoBall.cs
+++ Slider/Assets/Scripts/Map/Desert/CasinoDiscoBall.cs
@@ -9,9 +9,25 @@ public class CasinoDiscoBall : MonoBehaviour, ISavable
     public Collider2D ballCollider;
     private bool didFall;
 
+    [Header("External References")]
+    public DiceGizmo dice1;
+    public DiceGizmo dice2;
+
     private const string SAVE_STRING = "desertDiscoBallFell";
+    private const string CHAD_FLAG1 = "desertDiscoBallContactGround";
+    private const string CHAD_FLAG2 = "desertSlideAfterBallFell";
     private const string FALL_ANIMTION = "Fall";
     private const string BROKEN_IDLE_ANIMTION = "BrokenIdle";
+
+    private void OnEnable() 
+    {
+        SGridAnimator.OnSTileMoveEnd += OnSTileMoveEnd;
+    }
+
+    private void OnDisable() 
+    {
+        SGridAnimator.OnSTileMoveEnd -= OnSTileMoveEnd;
+    }
     
     public void Load(SaveProfile profile)
     {
@@ -52,5 +68,34 @@ public class CasinoDiscoBall : MonoBehaviour, ISavable
     {
         AudioManager.Play("Slide Explosion");
         CameraShake.Shake(1f, 0.5f); // values from Anchor
+
+        // for dice
+        SaveSystem.Current.SetBool(CHAD_FLAG1, true);
+        dice2.changeValue(1);
+    }
+
+
+
+    public void OnSTileMoveEnd(object sender, SGridAnimator.OnTileMoveArgs e)
+    {
+        if ((e.stile.islandId == 5 || e.stile.islandId == 6) && didFall)
+        {
+            SaveSystem.Current.SetBool(CHAD_FLAG2, true);
+        }
+    }
+
+    public void DoDiceCutscene()
+    {
+        StartCoroutine(IDoDiceCutscene());
+    }
+
+    private IEnumerator IDoDiceCutscene()
+    {
+        dice1.changeValue(5);
+        dice2.changeValue(4);
+
+        yield return new WaitForSeconds(2.5f);
+
+        Fall();
     }
 }
--- Slider/Assets/Scripts/Map/Props/DiceGizmo.cs
+++ Slider/Assets/Scripts/Map/Props/DiceGizmo.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class DiceGizmo : MonoBehaviour
+public class DiceGizmo : MonoBehaviour, ISavable
 {
-    public STile myStile;
+    [HideInInspector] public STile myStile;
+    [SerializeField] private string saveString;
     [SerializeField] private int diceIndex;
     [SerializeField] private bool onlySoundInHouse;
     [SerializeField] private DiceGizmo otherDice;
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private TextMeshProUGUI bgText;
     [SerializeField] private Transform baseTransform;
+    [SerializeField] private SpriteRenderer sr;
 
     public int value;
     public Sprite[] sprites;
     public Sprite[] rotatedSprites;
+    public Sprite[] bumpSprites;
 
-    private SpriteRenderer sr;
     private Coroutine diceShakeCoroutine;
     //public Animator animator; // this is only based on Tree animator controller rn
     //Chen: Should the above be changed to the Dice animator controller or something?
@@ -29,7 +31,6 @@ public class DiceGizmo : MonoBehaviour
         {
             FindSTile();
         }
-        sr = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -44,6 +45,20 @@ public class DiceGizmo : MonoBehaviour
         if (myStile != null)
             SGridAnimator.OnSTileMoveEnd -= OnStileChangeDir;
         Anchor.OnAnchorInteract -= UpdateDiceOnAnchorInteract;
+    }
+    
+    public void Load(SaveProfile profile)
+    {
+        value = profile.GetInt(saveString, 1);
+        UpdateSprite();
+
+        text.text = value.ToString();
+        bgText.text = value.ToString();
+    }
+
+    public void Save()
+    {
+        SaveSystem.Current.SetInt(saveString, value);
     }
 
     public void CheckElevens(Condition c)
@@ -68,13 +83,49 @@ public class DiceGizmo : MonoBehaviour
             else AudioManager.PlayWithPitch("Dice Shake", 0.75f);
         }
         
-        sr.sprite = sprites[value - 1];
+        UpdateSprite();
 
         text.text = "";
         bgText.text = "";
 
         if (diceShakeCoroutine != null) StopCoroutine(diceShakeCoroutine);
         diceShakeCoroutine = StartCoroutine(Shake(7));
+    }
+
+    public void BumpValue()
+    {
+        animationFinished = false;
+        value += 1;
+        if (value >= 7) value = 1;
+
+        if (!onlySoundInHouse || (onlySoundInHouse && Player.GetIsInHouse()))
+        {
+            if (value != 1) AudioManager.Play("Dice Bump");
+            else AudioManager.PlayWithPitch("Dice Bump", 0.75f);
+        }
+        
+        UpdateSprite();
+
+        text.text = "";
+        bgText.text = "";
+
+        if (diceShakeCoroutine != null) StopCoroutine(diceShakeCoroutine);
+        diceShakeCoroutine = StartCoroutine(AnimateBump());
+    }
+
+    private IEnumerator AnimateBump()
+    {
+        for (int i = 0; i < bumpSprites.Length; i++)
+        {
+            sr.sprite = bumpSprites[i];
+            yield return new WaitForSeconds(0.125f);
+        }
+        UpdateSprite();
+
+        text.text = value.ToString();
+        bgText.text = value.ToString();
+        animationFinished = true;
+        diceShakeCoroutine = null;
     }
 
     public IEnumerator Shake(int times)
@@ -92,7 +143,7 @@ public class DiceGizmo : MonoBehaviour
         yield return new WaitForSeconds(.25f);
 
         transform.position = baseTransform.position;
-        sr.sprite = sprites[value - 1];
+        UpdateSprite();
 
         text.text = value.ToString();
         bgText.text = value.ToString();
@@ -100,12 +151,17 @@ public class DiceGizmo : MonoBehaviour
         diceShakeCoroutine = null;
     }
 
+    public void UpdateSprite()
+    {
+        sr.sprite = sprites[value - 1];
+    }
+
 
     public void OnStileChangeDir(object sender, SGridAnimator.OnTileMoveArgs e)
     {
         if (e.stile.islandId == myStile.islandId)
         {
-            changeValue(value + 1);
+            BumpValue();
         }
     }
 
@@ -119,23 +175,24 @@ public class DiceGizmo : MonoBehaviour
 
     private void FindSTile()
     {
-        Transform curr = transform;
-        int i = 0;
-        while (curr.parent != null && i < 100)
-        {
-            if (curr.GetComponent<STile>() != null)
-            {
-                myStile = curr.GetComponent<STile>();
-                return;
-            }
+        // Transform curr = transform;
+        // int i = 0;
+        // while (curr.parent != null && i < 100)
+        // {
+        //     if (curr.GetComponent<STile>() != null)
+        //     {
+        //         myStile = curr.GetComponent<STile>();
+        //         return;
+        //     }
 
-            // Debug.Log(curr.name);
-            curr = curr.parent;
-            i += 1;
-        }
+        //     // Debug.Log(curr.name);
+        //     curr = curr.parent;
+        //     i += 1;
+        // }
 
-        if (i == 100)
-            Debug.LogWarning("something went wrong in finding stile!");
+        myStile = transform.GetComponentInParent<STile>();
+
+        if (myStile == null)
+            Debug.LogWarning("Something went wrong in finding stile!");
     }
-
 }
