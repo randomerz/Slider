f6dbfbdd29f5b93949e25f53fe5391d0cac79414
ChaseOB <58964553+ChaseOB@users.noreply.github.com>
2023-05-21 16:00:00 -0400

    rat hotfix

--- Slider/Assets/Scripts/AI/Rat/RatAI.cs
+++ Slider/Assets/Scripts/AI/Rat/RatAI.cs
@@ -156,7 +156,7 @@ public class RatAI : MonoBehaviour, ISavable
         {
             transform.SetParent(currentStileUnderneath.transform);
             visitedTiles.Add(currentStileUnderneath.islandId);
-            if(!hasAchievement && visitedTiles.Count >= SGrid.Current.GetNumTilesCollected()) {
+            if(!hasAchievement && visitedTiles.Count >= Mathf.Max(7, SGrid.Current.GetNumTilesCollected())) {
                 AchievementManager.SetAchievementStat("completedRatAllTiles", 1);
                 hasAchievement = true;
             }
