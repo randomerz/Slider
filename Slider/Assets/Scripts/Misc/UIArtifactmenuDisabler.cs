b904152e50bc36343330185587e8bedaf053a779
Drew <drew.j.busch@gmail.com>
2023-05-30 01:19:50 -0400

    unbalmer peak

--- Slider/Assets/Scripts/Misc/UIArtifactmenuDisabler.cs
+++ Slider/Assets/Scripts/Misc/UIArtifactmenuDisabler.cs
@@ -8,28 +8,8 @@ public class UIArtifactmenuDisabler : MonoBehaviour
 {
     public UIArtifactMenus uiArtifactMenus;
 
-    [SerializeField] private OceanControllerSupportButtonsHolder oceanControllerSupportButtonsHolder;
-    [SerializeField] private Button topLeftControllerButton;
-
     public void DisableArtPanel()
     {
         uiArtifactMenus.DisableArtPanel();
-    }
-
-
-    private void OnEnable()
-    {
-        //sorry this is kinda cringe
-        if (oceanControllerSupportButtonsHolder!= null) 
-        {
-            if (Player.GetInstance().GetCurrentControlScheme() == "Controller")
-            {
-                oceanControllerSupportButtonsHolder.gameObject.SetActive(true);
-                topLeftControllerButton.Select();
-                //Debug.LogError("Control SELECT!!");
-                oceanControllerSupportButtonsHolder.ColorAllButtonsBasedOnIfSelected();
-            }
-            else { oceanControllerSupportButtonsHolder.gameObject.SetActive(false); }
-        }
     }
 }
