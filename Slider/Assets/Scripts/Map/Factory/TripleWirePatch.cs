11157021f956babf5cb8c2875d289be9dd18b04b
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2023-07-07 23:41:14 -0700

    Factory Scene Changes for TG Bug Fix

--- Slider/Assets/Scripts/Map/Factory/TripleWirePatch.cs
+++ Slider/Assets/Scripts/Map/Factory/TripleWirePatch.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 // Basically is a power source that will turn on if the gate is on
 
 //Shouldn't need this, but I'm leaving it in until my fix is stable.
-public class TripleWirePatch : ElectricalNode
-{
-    public TimedGate gateToWatch;
-    private bool signalStarted;
+//public class TripleWirePatch : ElectricalNode
+//{
+//    public TimedGate gateToWatch;
+//    private bool signalStarted;
     
-    protected override void Update()
-    {
-        base.Update();
+//    protected override void Update()
+//    {
+//        base.Update();
 
-        if (gateToWatch.Powered && !signalStarted)
-        {
-            signalStarted = true;
-            StartSignal(true);
-        }
-    }
-}
+//        if (gateToWatch.Powered && !signalStarted)
+//        {
+//            signalStarted = true;
+//            StartSignal(true);
+//        }
+//    }
+//}
