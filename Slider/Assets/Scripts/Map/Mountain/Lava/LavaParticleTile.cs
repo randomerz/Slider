fa09c755618c6c26396193876caf9c01846a14a1
chase.obrien <chaseobrien128@gmail.com>
2023-07-08 22:54:20 -0400

    oh boy im about to blow up the particle script

--- Slider/Assets/Scripts/Map/Mountain/Lava/LavaParticleTile.cs
+++ Slider/Assets/Scripts/Map/Mountain/Lava/LavaParticleTile.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System;
-public class LavaParticleTile
+
+public class LavaParticleTile : MonoBehaviour
 {
     [Serializable]
     public class LavaParticleEmitter
     {
         public ParticleSystem particleSystem;
+        public bool dynamic;
     }
+
+    public LavaParticleEmitter[] emitters = new LavaParticleEmitter[4]; //0 1 2 3 = E N W S
 }
