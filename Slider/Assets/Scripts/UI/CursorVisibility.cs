621be5ad8dae4cf296a773b0b2acb17306ecbc33
chase.obrien <chaseobrien128@gmail.com>
2023-08-04 10:57:28 -0400

    Fix dither, cursor, rails on ice

--- Slider/Assets/Scripts/UI/CursorVisibility.cs
+++ Slider/Assets/Scripts/UI/CursorVisibility.cs
@@ -6,7 +6,7 @@ public class CursorVisibility : MonoBehaviour
 {
     private void OnGUI() {
         bool cursorActive = (UIManager.IsUIOpen() || UIArtifactMenus.IsArtifactOpen() || GameUI.instance.isMenuScene);
-        cursorActive = !SettingsManager.HideCursor || cursorActive;
+        cursorActive = Player.GetInstance().GetCurrentControlScheme() != "Controller" && (!SettingsManager.HideCursor || cursorActive);
         Cursor.visible = cursorActive;
         Cursor.lockState = cursorActive ? CursorLockMode.None : CursorLockMode.Confined;
     }
