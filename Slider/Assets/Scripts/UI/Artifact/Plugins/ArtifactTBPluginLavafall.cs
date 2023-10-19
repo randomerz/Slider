9ab293205d12c279aaa67bfd56c19fe8df9a8e4d
chase.obrien <chaseobrien128@gmail.com>
2023-10-18 22:42:42 -0400

    lavafall ui

---
+++ Slider/Assets/Scripts/UI/Artifact/Plugins/ArtifactTBPluginLavafall.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactLavafallPlugin : ArtifactTBPlugin
+{
+    public UISpriteSwapper spriteSwapper;
+
+    private void OnEnable()
+    {
+        UpdateUI();
+    }
+
+    public override void OnPosChanged()
+    {
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        bool shouldBeActive = true; 
+        if(button.y != 3)
+            shouldBeActive = false;
+        if(UIArtifact.GetButton(button.x, 2) != null && UIArtifact.GetButton(button.x, 2).TileIsActive)
+            shouldBeActive = false;
+        if(shouldBeActive)
+            spriteSwapper.TurnOn();
+        else
+            spriteSwapper.TurnOff();
+    }
+}
