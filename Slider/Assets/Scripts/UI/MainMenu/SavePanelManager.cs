274fc14455fd34a79925d19a4f2722b924bc416b
Daniel Carr <randomerz56@gmail.com>
2023-12-11 21:47:10 -0500

    fix build error from imported package

--- Slider/Assets/Scripts/UI/MainMenu/SavePanelManager.cs
+++ Slider/Assets/Scripts/UI/MainMenu/SavePanelManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 
 public class SavePanelManager : MonoBehaviour
