a34658f8bbf643bc5ae5286e117596d9bd6fe793
chase.obrien <chaseobrien128@gmail.com>
2024-01-10 21:45:06 -0500

    GTA 6 pre alpha

--- Slider/Assets/Scripts/Map/Desert/DesertGTA.cs
+++ Slider/Assets/Scripts/Map/Desert/DesertGTA.cs
@@ -4,50 +4,18 @@ using UnityEngine;
 
 public class DesertGTA : MonoBehaviour, ISavable
 {
-    [SerializeField] private bool explodedWall = false;
-    [SerializeField] private bool explosivesPlaced = false;
+    private bool explodedWall = false;
+    private bool explosivesPlaced = false;
     [SerializeField] private List<GameObject> placableExplosives;
-    [SerializeField] private List<GameObject> removeAfterExplosion;
-    [SerializeField] private GameObject explosivesPlaceConditional;
+    [SerializeField] private GameObject laserCollider;
+    public List<GameObject> removeAfterExplosion;
     public PlayerConditionals bombSignConditional;
+    private bool isExploding;
 
-
-    public void PlaceExplosives()
-    {
-        if(explosivesPlaced) return;
-
-        explosivesPlaced = true;
-        foreach(GameObject g in placableExplosives)
-        {
-            g.SetActive(true);
-        }
-        AudioManager.Play("Hat Click");
-        bombSignConditional.DisableConditionals();
-    }
-
-    public void ExplodeCasinoWall()
-    {
-        StartCoroutine(ExplodeCasinoWallCoroutine());
-    }
-
-    private IEnumerator ExplodeCasinoWallCoroutine()
-    {
-        yield return null;
-        EndExplosion();
-    }
-
-    public void EndExplosion(bool fromSave = false)
-    {
-        foreach(GameObject g in removeAfterExplosion)
-        {
-            g.SetActive(false);
-        }
-        explodedWall = true;
-    }
-
+    
     public void Save()
     {
-        throw new System.NotImplementedException();
+        SaveSystem.Current.SetBool("DesertBlewUpCasinoWall", explodedWall);
     }
 
     public void Load(SaveProfile profile)
@@ -59,8 +27,57 @@ public class DesertGTA : MonoBehaviour, ISavable
         }
     }
 
+
+    public void PlaceExplosives()
+    {
+        if(explosivesPlaced) return;
+        //if(!PlayerInventory.Contains("Explosives", Area.Military)) return;
+
+        explosivesPlaced = true;
+        foreach(GameObject g in placableExplosives)
+        {
+            g.SetActive(true);
+        }
+        AudioManager.Play("Hat Click");
+        bombSignConditional.DisableConditionals();
+        laserCollider.SetActive(true);
+    }
+
+    public void ExplodeCasinoWall()
+    {
+        StartCoroutine(ExplodeCasinoWallCoroutine());
+    }
+
+    private IEnumerator ExplodeCasinoWallCoroutine()
+    {
+        isExploding = true;
+        AudioManager.Play("Slide Explosion");
+        yield return new WaitForSeconds(2);
+        EndExplosion();
+    }
+
+    public void EndExplosion(bool fromSave = false)
+    {
+        foreach(GameObject g in removeAfterExplosion)
+        {
+            g.SetActive(false);
+        }
+        foreach(GameObject g in placableExplosives)
+        {
+            g.SetActive(false);
+        }
+        laserCollider.SetActive(false);
+        explodedWall = true;
+        isExploding = false;
+    }
+
     public void BlewUpCasinoWall(Condition c)
     {
         c.SetSpec(explodedWall);
+    }
+
+    public void IsBlowingUpCasinoWall(Condition c)
+    {
+        c.SetSpec(isExploding);
     }
 }
