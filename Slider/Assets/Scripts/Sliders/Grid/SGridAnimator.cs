1e1f8c7bf98c7856537aee2bc84d05911306985a
chase.obrien <chaseobrien128@gmail.com>
2024-01-10 22:25:23 -0500

    stuff

--- Slider/Assets/Scripts/Sliders/Grid/SGridAnimator.cs
+++ Slider/Assets/Scripts/Sliders/Grid/SGridAnimator.cs
@@ -17,7 +17,6 @@ public class SGridAnimator : MonoBehaviour
     public static event System.EventHandler<OnTileMoveArgs> OnSTileMoveEndEarly;
     public static event System.EventHandler<OnTileMoveArgs> OnSTileMoveEnd;
     public static event System.EventHandler<OnTileMoveArgs> OnSTileMoveEndLate; //:clown emoji
-   // public static event System.EventHandler<OnTileMoveArgs> OnSTileMoveEndLateLate; //got lotion on my tile, strokin my tile
 
     // set in inspector
     public AnimationCurve movementCurve;
@@ -190,14 +189,6 @@ public class SGridAnimator : MonoBehaviour
             smove = move,
             moveDuration = currMoveDuration
         });
-
-        // OnSTileMoveEndLateLate?.Invoke(this, new OnTileMoveArgs
-        // {
-        //     stile = stile,
-        //     prevPos = moveCoords.startLoc,
-        //     smove = move,
-        //     moveDuration = currMoveDuration
-        // });
 
         EffectOnMoveFinish(move, moveCoords, isPlayerOnStile ? null : stile.transform, stile, playSound);
     }
