3e14116b6e53e5778690bddc6acee570edd02408
chase.obrien <chaseobrien128@gmail.com>
2024-01-13 18:15:28 -0500

    give desert tiles if from magitech

--- Slider/Assets/Scripts/Sliders/Grid/AreaSTiles/MirageSTileManager.cs
+++ Slider/Assets/Scripts/Sliders/Grid/AreaSTiles/MirageSTileManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 using System.Text;
 
 public class MirageSTileManager : Singleton<MirageSTileManager>, ISavable
