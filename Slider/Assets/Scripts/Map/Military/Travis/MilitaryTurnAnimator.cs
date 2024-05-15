7938c2f9cf234c70bd35db304c3087eeecc4e848
boomo <randomerz56@gmail.com>
2024-05-14 23:31:19 -0400

    factory queue boilerplate

--- Slider/Assets/Scripts/Map/Military/Travis/MilitaryTurnAnimator.cs
+++ Slider/Assets/Scripts/Map/Military/Travis/MilitaryTurnAnimator.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 
 public class MilitaryTurnAnimator : Singleton<MilitaryTurnAnimator>
 {
-    public Queue<MGMove> moveQueue = new();
+    public enum QueueStatus 
+    {
+        Off,
+        Ready, // Might still be processing but send the next
+        Processing, // Proccessing animations
+    }
+
+    private Queue<MGMove> moveQueue = new();
+    private List<MGMove> activeMoves = new();
+    private QueueStatus status = QueueStatus.Off;
 
     private void Awake()
     {
@@ -12,8 +21,30 @@ public class MilitaryTurnAnimator : Singleton<MilitaryTurnAnimator>
 
     public static void AddNewMove(MGMove move)
     {
-        // _instance.moveQueue.Enqueue(move);
-        _instance.ExecuteMove(move);
+        _instance.moveQueue.Enqueue(move);
+        _instance.CheckQueue();
+    }
+
+    private void CheckQueue()
+    {
+        if (moveQueue.Count == 0)
+        {
+            return;
+        }
+
+        switch (status)
+        {
+            case QueueStatus.Off:
+            case QueueStatus.Ready:
+                MGMove nextMove = moveQueue.Dequeue();
+                activeMoves.Add(nextMove);
+                ExecuteMove(nextMove);
+                status = QueueStatus.Processing;
+                break;
+            case QueueStatus.Processing:
+                // Queue is processing -- do nothing!
+                break;
+        }
     }
 
     private void ExecuteMove(MGMove move)
@@ -30,5 +61,13 @@ public class MilitaryTurnAnimator : Singleton<MilitaryTurnAnimator>
 
         move.unit.NPCController.SetPosition(targetPos);
         move.unit.AttachedSTile = move.endStile;
+
+        FinishMove(move);
+    }
+
+    private void FinishMove(MGMove move)
+    {
+        activeMoves.Remove(move);
+        status = moveQueue.Count == 0 ? QueueStatus.Off : QueueStatus.Processing;
     }
 }
