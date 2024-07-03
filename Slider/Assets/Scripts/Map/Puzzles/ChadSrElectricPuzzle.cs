c6de2f3f7228c92a81c47bce24ea300ee42cd892
boomo <randomerz56@gmail.com>
2024-07-03 00:38:03 -0400

    Factory polish and bug fixes

--- Slider/Assets/Scripts/Map/Puzzles/ChadSrElectricPuzzle.cs
+++ Slider/Assets/Scripts/Map/Puzzles/ChadSrElectricPuzzle.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class ChadSrElectricPuzzle : MonoBehaviour
 {
+    private const string CHAD_SR_INTRO = "FactoryPastChadSrIntro";
+    private const string CHAD_SR_MOVED_TILE = "FactoryPastChadSrMovedTile";
+
     [SerializeField] private Item itemHolding;
     [SerializeField] private Transform itemHolderTransform;
     [SerializeField] private ConductiveElectricalNode itemHoldingNode;
     [SerializeField] private Collider2D itemElectricalTrigger;
     [SerializeField] private Transform dropItemLoc;
 
-    private bool updateTransformLateStart;
-
     private void OnEnable()
     {
         itemHoldingNode?.OnPoweredOn.AddListener(DropItemHolding);
+        SGridAnimator.OnSTileMoveEnd += OnSTileMove;
     }
 
     private void OnDisable()
     {
         itemHoldingNode?.OnPoweredOn.RemoveListener(DropItemHolding);
+        SGridAnimator.OnSTileMoveEnd -= OnSTileMove;
     }
 
     private void Start()
@@ -67,5 +70,13 @@ public class ChadSrElectricPuzzle : MonoBehaviour
     public void CheckPuzzleComplete(Condition cond)
     {
         cond.SetSpec(SaveSystem.Current.GetBool("ChadSrPuzzleComplete"));
+    }
+
+    private void OnSTileMove(object sender, SGridAnimator.OnTileMoveArgs e)
+    {
+        if (SaveSystem.Current.GetBool(CHAD_SR_INTRO))
+        {
+            SaveSystem.Current.SetBool(CHAD_SR_MOVED_TILE, true);
+        }
     }
 }
