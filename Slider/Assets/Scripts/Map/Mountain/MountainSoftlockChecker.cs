5c3a6db7d1826bff271622efec3022b4e4f27d75
chase.obrien <chaseobrien128@gmail.com>
2024-07-24 22:33:26 -0400

    Update MountainSoftlockChecker.cs

--- Slider/Assets/Scripts/Map/Mountain/MountainSoftlockChecker.cs
+++ Slider/Assets/Scripts/Map/Mountain/MountainSoftlockChecker.cs
@@ -51,6 +51,7 @@ public class MountainSoftlockChecker : MonoBehaviour
 
     private bool AnchorInBoxOfSadness()
     {
+        if(anchor == null) return false;
         return PosInBoxOfSadness(anchor.transform.position);
     }
 
