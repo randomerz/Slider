a2f6da07e36dc714fecb34ff4d679804b6c06540
Daniel Carr <randomerz56@gmail.com>
2025-06-21 19:14:21 -0400

    Add text bg to world map screen

--- Slider/Assets/Scripts/NPCs/DialogueContrastBackground.cs
+++ Slider/Assets/Scripts/NPCs/DialogueContrastBackground.cs
@@ -7,15 +7,36 @@ using TMPro;
 public class DialogueContrastBackground : MonoBehaviour
 {
     public float borderWidth = 0.5f;
-    private Vector3 offset = new Vector3(0, 0.6f);
+    public Vector3 offset = new Vector3(0, 0.6f);
 
     public TextMeshProUGUI text;
     public RectTransform backgroundRectTransform;
 
-    private void Update() 
+    public bool toggleImageBasedOnSettings = false; // if you want to this script to handle itself,
+                                                    // NPCs handle it in DialogueDisplay.cs
+    public Image imageToToggle;
+
+    private void Update()
     {
+        if (toggleImageBasedOnSettings)
+        {
+            CheckContrast();
+        }
+
         Bounds bounds = text.bounds;
         backgroundRectTransform.anchoredPosition = bounds.center + offset;
         backgroundRectTransform.sizeDelta = 2 * (bounds.extents + new Vector3(borderWidth, borderWidth));
+    }
+
+    private void CheckContrast()
+    {
+        if (imageToToggle == null)
+        {
+            return;
+        }
+
+        bool highContrastMode = SettingsManager.Setting<bool>(Settings.HighContrastTextEnabled).CurrentValue;
+        bool emptyMessage = text.text.Trim().Length == 0;
+        imageToToggle.enabled = highContrastMode && !emptyMessage;
     }
 }
